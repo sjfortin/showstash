5ef29d3ba74235c4646fd0cbe712ec376e26d79a
Sam Fortin <sam.j.fortin@gmail.com>
2017-09-11 13:29:12 -0500

    Simplify database structure

--- database.sql
+++ database.sql
@@ -6,38 +6,32 @@ CREATE TABLE users
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL
 );
-CREATE TABLE shows
-(
-    id SERIAL PRIMARY KEY,
-    band VARCHAR(80) NOT NULL,
-    show_date DATE NOT NULL,
-    venue VARCHAR(120) NOT NULL,
-    venue_id INT REFERENCES venues
-);
-CREATE TABLE users_shows
-(
-    id SERIAL PRIMARY KEY,
-    notes VARCHAR(256),
-    show_id INT REFERENCES shows,
-    user_id INT REFERENCES users
-);
 CREATE TABLE venues
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
     city VARCHAR(80) NOT NULL,
-    state VARCHAR(10),
+    state VARCHAR(10)
 );
-CREATE TABLE friends
+CREATE TABLE users_shows
 (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(80) NOT NULL,
-    last_name VARCHAR(80) NOT NULL,
-    user_id INT REFERENCES users
+    band VARCHAR(80) NOT NULL,
+    show_date DATE NOT NULL,
+    notes VARCHAR(256),
+    user_id INT REFERENCES users,
+    venue_id INT REFERENCES venues
 );
-CREATE TABLE user_show_friends
-(
-    user_show_id INT REFERENCES users_shows,
-    friend_id INT REFERENCES friends,
-    PRIMARY KEY (user_show_id, friend_id)
-);
+-- CREATE TABLE friends
+-- (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(80) NOT NULL,
+--     last_name VARCHAR(80) NOT NULL,
+--     user_id INT REFERENCES users
+-- );
+-- CREATE TABLE user_show_friends
+-- (
+--     user_show_id INT REFERENCES users_shows,
+--     friend_id INT REFERENCES friends,
+--     PRIMARY KEY (user_show_id, friend_id)
+-- );
