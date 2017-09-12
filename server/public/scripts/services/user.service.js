f9822def29bf1b61879fc438f03ee7554b54a28c
Sam Fortin <sam.j.fortin@gmail.com>
2017-09-12 11:24:15 -0500

    UserService refactored to a service from a factory

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -1,30 +1,31 @@
-app.factory('UserService', function($http, $location){
+app.service('UserService', function ($http, $location) {
 
-  var userObject = {};
+  var self = this;
 
-  return {
-    userObject : userObject,
+  self.userObject = {
 
-    getuser : function(){
-      $http.get('/user').then(function(response) {
-          if(response.data.email) {            
-              // user has a curret session on the server
-              userObject.email = response.data.email;
-              userObject.first_name = response.data.first_name;
-              userObject.last_namep = response.data.last_namep;
-          } else {
-              // user has no session, bounce them back to the login page
-              $location.path("/home");
-          }
-      },function(response){
-        $location.path("/home");
-      });
-    },
-
-    logout : function() {
-      $http.get('/user/logout').then(function(response) {
-        $location.path("/home");
-      });
-    }
   };
+
+  self.getuser = function () {
+    $http.get('/user').then(function (response) {
+      if (response.data.email) {
+        // user has a curret session on the server
+        self.userObject.email = response.data.email;
+        self.userObject.first_name = response.data.first_name;
+        self.userObject.last_name = response.data.last_name;
+      } else {
+        // user has no session, bounce them back to the login page
+        $location.path("/home");
+      }
+    }, function (response) {
+      $location.path("/home");
+    });
+  }
+
+  self.logout = function () {
+    $http.get('/user/logout').then(function (response) {
+      $location.path("/home");
+    });
+  }
+
 });
