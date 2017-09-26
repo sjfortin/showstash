560fc2d064dd4a5ce77540439ed986ce4fe3a318
Sam Fortin <sam.j.fortin@gmail.com>
2017-09-25 21:56:57 -0500

    Delete config

--- server/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    songKickApiKey: 'ADoVbyxu3UwK6nJM',
-    setlistApiKey: 'e79a7647-93fd-43c2-8df4-294e266401d6',
-    lastFmApiKey: '17fc707735a64230fe40a6576b115c6a'
-}
-
-module.exports = config;
