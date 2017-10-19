f61508b4f5613aa778bfb2062c52f91fa07a9938
Sam Fortin <sam.j.fortin@gmail.com>
2017-10-19 14:58:37 -0500

    Manual login readded

--- server/public/scripts/services/artist-image.service.js
+++ server/public/scripts/services/artist-image.service.js
@@ -17,16 +17,15 @@ app.service('ArtistImageService', ['$http', function ($http) {
                 return image.image != '' && image.image != 'https://i.pinimg.com/originals/9d/23/17/9d2317310b456185ed9663d3d7b87490.jpg';
             });
             self.artistImages.list.forEach(function(show){
-                show.homeImageSize = randomImageWidth(40, 100);
-            })
-            console.log('self.artistImages.list', self.artistImages.list);
-            
+                show.homeImageSize = randomImageWidth(30, 100);
+            });            
         });
     };
 
     // run getArtistImages on service load
     self.getArtistImages();
 
+    // used to retrieve a random number that will be used for a random image width on the home page
     function randomImageWidth(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
